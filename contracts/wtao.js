025f2fdff33f0bdf4fe42a303ab47acb03a68f5c
Salatik-N <nikita-salata@mail.ru>
2024-05-20 13:11:48 +0200

    ADD: wtao staking

--- contracts/wtao.js
+++ contracts/wtao.js
@@ -1,5 +1,5 @@
 const { Web3 } = require("web3");
-const abi = require("../abi.json");
+const abiWTAO = require("../abiWTAO.json");
 
 const web3Provider = new Web3.providers.WebsocketProvider(
   `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_TOKEN}`
@@ -7,6 +7,6 @@ const web3Provider = new Web3.providers.WebsocketProvider(
 const web3 = new Web3(web3Provider);
 
 const address = "0x3E0858F65aBF8606103f2c6B98138E4208cC795B";
-const contract = new web3.eth.Contract(abi, address);
+const contract = new web3.eth.Contract(abiWTAO, address);
 
 module.exports = contract;
