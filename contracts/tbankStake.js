6e06b7234c7b46b4b076d0f4057ed1bdb644000a
Salatik-N <nikita-salata@mail.ru>
2024-06-03 23:22:45 +0200

    EDIT: web3 provider

--- contracts/tbankStake.js
+++ contracts/tbankStake.js
@@ -3,8 +3,38 @@ const { Web3 } = require("web3");
 const abiStakeTBANK = require("../abiStakeTBANK.json");
 
 const web3Provider = new Web3.providers.WebsocketProvider(
-  `wss://arbitrum-mainnet.infura.io/ws/v3/${process.env.INFURA_TOKEN}`
+  `wss://arbitrum-mainnet.infura.io/ws/v3/${process.env.INFURA_TOKEN}`,
+  {
+    timeout: 30000,
+
+    clientConfig: {
+      // Useful if requests are large
+      maxReceivedFrameSize: 100000000,
+      maxReceivedMessageSize: 100000000,
+
+      // Useful to keep a connection alive
+      keepalive: true,
+      keepaliveInterval: -1,
+    },
+
+    // Enable auto reconnection
+    reconnect: {
+      auto: true,
+      delay: 1000,
+      maxAttempts: 10,
+      onTimeout: false,
+    },
+  }
 );
+web3Provider.on("close", (event) => {
+  console.log(event);
+  console.log("Websocket closed.");
+});
+
+web3Provider.on("error", (error) => {
+  console.error(error);
+});
+
 const web3 = new Web3(web3Provider);
 
 const address = "0xfce658b6e7B93F9c8281bbFd93394fBfd04A1402";
